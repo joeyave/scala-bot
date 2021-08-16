49f7750cf6085ecd2ef7ffdaca2d3402a119e405
Joseph <aveltsev@gmail.com>
2021-08-16 14:42:58 +0300

    Update handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,7 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/api/drive/v3"
-	"log"
+        "github.com/rs/zerolog/log"
 	"regexp"
 	"strconv"
 	"strings"
