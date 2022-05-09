92eab63e0d56665eaabe504215c1e46b04cd2106
josephaveltsev <aveltsev@gmail.com>
2022-05-09 22:00:09 +0300

    fixes bug with user notification updates go to 1.18

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/joeyave/scala-bot
 
-go 1.18
+go 1.17
 
 require (
 	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.7
--- util/helper.go
+++ util/helper.go
@@ -29,9 +29,9 @@ func File(bot *gotgbot.Bot, file *gotgbot.File) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-func SplitKeyboardToColumns[T gotgbot.KeyboardButton | gotgbot.InlineKeyboardButton](k [][]T, colNum int) [][]T {
+func SplitKeyboardToColumns(k [][]gotgbot.KeyboardButton, colNum int) [][]gotgbot.KeyboardButton {
 
-	var newK [][]T
+	var newK [][]gotgbot.KeyboardButton
 	var i int
 
 	for _, row := range k {
@@ -41,7 +41,30 @@ func SplitKeyboardToColumns[T gotgbot.KeyboardButton | gotgbot.InlineKeyboardBut
 			}
 
 			if i == 0 {
-				newK = append(newK, []T{button})
+				newK = append(newK, []gotgbot.KeyboardButton{button})
+			} else if i < colNum {
+				newK[len(newK)-1] = append(newK[len(newK)-1], button)
+			}
+			i++
+		}
+	}
+
+	return newK
+}
+
+func SplitInlineKeyboardToColumns(k [][]gotgbot.InlineKeyboardButton, colNum int) [][]gotgbot.InlineKeyboardButton {
+
+	var newK [][]gotgbot.InlineKeyboardButton
+	var i int
+
+	for _, row := range k {
+		for _, button := range row {
+			if i == colNum {
+				i = 0
+			}
+
+			if i == 0 {
+				newK = append(newK, []gotgbot.InlineKeyboardButton{button})
 			} else if i < colNum {
 				newK[len(newK)-1] = append(newK[len(newK)-1], button)
 			}
